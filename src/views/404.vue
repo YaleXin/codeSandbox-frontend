c592c28a31a9a189ba7b5da9f5414f1d8b67b161
YaleXin <495592364@qq.com>
2024-07-12 22:26:44 +0800

    优化404页面

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
-    
-    <h1 > 404</h1>
-    </div>
+    <a-row justify="center">
+      <a-col :span="8">
+        <a-card :style="{ width: '100%' }" hoverable>
+          <a-result status="warning" title="404">
+            <template #subtitle> 您似乎来到了错误的页面~~ </template>
+          </a-result>
+        </a-card>
+      </a-col>
+    </a-row>
+  </div>
 </template>
 
 <script setup lang="ts">
-
 </script>
